b2dad59567147ef11ded73f1d9174452bb9daea1
viltt-0266 <le.thi.tuong.vi@sun-asterisk.com>
2023-08-04 13:35:03 +0700

    s

--- Tour_Booking/booking/settings.py
+++ Tour_Booking/booking/settings.py
@@ -28,18 +28,17 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Trong settings.py
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
+DEBUG = True
 
 DATABASE_URL = 'mysql://root:22102002@localhost:3306/my_booking'
 
-ALLOWED_HOSTS = ["tourbookingdeploy-production.up.railway.app",'https://*.127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ["https://tourbookingdeploy-production.up.railway.app",'https://*.127.0.0.1']
+ALLOWED_HOSTS = ["web-vivivideploy.up.railway.app",'https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ["https://web-vivivideploy.up.railway.app",'https://*.127.0.0.1']
 
 CORS_ALLOWED_ORIGINS = [
-    "https://tourbookingdeploy-production.up.railway.app",
+    "https://web-vivivideploy.up.railway.app",
     # Add other trusted origins here if needed
 ]
-
-DEBUG = True
 
 # Application definition
 
@@ -59,6 +58,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
