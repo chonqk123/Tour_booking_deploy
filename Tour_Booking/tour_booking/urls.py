c0e1caa3e72bba993ca02577c8afd5e88f1216ff
chonqk123 <91725926+chonqk123@users.noreply.github.com>
2023-08-07 14:09:03 +0700

    demo project1

--- Tour_Booking/tour_booking/urls.py
+++ Tour_Booking/tour_booking/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from . import views
 from django.conf.urls.static import static
+from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -15,4 +17,8 @@ urlpatterns = [
     path("signup/send-mail-success/", views.send_mail_success, name="send-mail-success"),
     path('tour/<int:pk>/rate-comment/', views.tour_rating_comment, name='tour-rating-comment'),
     path('approve-tours/', views.approve_tours, name='approve-tours'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
